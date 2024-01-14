5330c48df6796e9d32c316dd596bb5eb48f8bbcd
Srinivasan T M <tmsrinivasantm@gmail.com>
2024-01-14 12:01:10 +0530

    hello rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -82,19 +82,27 @@ int main() {
 
 
     // operations
-    float vertices[] = {-0.5f, -0.5f, 0.0f,
-                         0.5f, -0.5f, 0.0f,
-                         0.0f,  0.5f, 0.0f};
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+    };
+    unsigned int indices[] = {0,1,3,1,2,3};
 
-    unsigned int vbo, vao;
+    unsigned int vbo, vao, ebo;
 
     glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
     glGenVertexArrays(1, &vao);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -109,7 +117,7 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 //      -----------------------------------------------
 
