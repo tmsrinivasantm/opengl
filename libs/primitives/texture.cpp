c814eb2039213c449c2361470c0ed7b7e1a8a8be
Srinivasan T M <tmsrinivasantm@gmail.com>
2024-01-15 19:46:22 +0530

    Flip texture direction

--- libs/primitives/texture.cpp
+++ libs/primitives/texture.cpp
@@ -4,6 +4,7 @@ namespace opengl {
 texture::texture(const std::string &filepath) {
     glGenTextures(1, &identifier);
     bind();
+    stbi_set_flip_vertically_on_load(true);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
